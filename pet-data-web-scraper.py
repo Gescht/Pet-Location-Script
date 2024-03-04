b30db998bca862a8aa616eb762da775598c11161
Gescht <tobias.alfons.tampe@gmail.com>
2024-03-04 21:29:13 +0100

    script can create an array: zoneID - mapID
    - zoneID is used on the webdatabase (url)
    - mapID is used by the addon (lua) ingame

--- pet-data-web-scraper.py
+++ pet-data-web-scraper.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+
+
+#get pet location data
 url = 'https://mop-shoot.tauri.hu/?npc=61367'
 soup = BeautifulSoup(requests.get(url).text, "html").find_all("script")
 for elemnt in soup:
