32394e2a25d0dc92da3284feee7ce2cb08346141
Gescht <tobias.alfons.tampe@gmail.com>
2024-03-04 16:25:43 +0100

    first webscrape and raw data extration
    - setup concept of scraping one webpage
    - navigate to the correct script element
    - extract raw data from element

---
+++ pet-data-web-scraper.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'https://mop-shoot.tauri.hu/?npc=61367'
+soup = BeautifulSoup(requests.get(url).text, "html").find_all("script")
+for elemnt in soup:
+    data = elemnt.text
+    if "g_mapperData" in data: 
+        print(data)
